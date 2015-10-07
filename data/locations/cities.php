059b6a0ee470b26f50521ecb9a6fa4fef109282e
Mikhail Vinogradov <michael@primordialsoup.ca>
2015-10-07 09:09:08 -0400

    adding countries, provinces, and cities son feeds

--- data/locations/cities.php
+++ data/locations/cities.php
@@ -1,3 +1,42 @@
-Cities JSON:<br/>
-Province: <?php echo $_GET['p']; ?><br/>
-Country: <?php echo $_GET['c']; ?>
+<?php
+
+include( 'json-header.php' );
+
+$province = strtoupper( $_GET['p'] );
+$country = strtoupper( $_GET['c'] );
+
+$cities = array();
+
+$row = 0;
+if ( ( $handle = fopen( $file, 'r' ) ) !== FALSE ) {
+    while ( ( $data = fgetcsv( $handle, 1000, ',' ) ) !== FALSE ) {
+        $num = count( $data );
+        $row++;
+        if( $row > 1 ) {
+            // get country code and compare
+            $cc = $data[4];
+            if( $country != $cc ) continue;
+            // get province code and compare
+            $prov = $data[6];
+            if( $province != $prov ) continue;
+            // get city code
+            $id = $data[0];
+            // add city to array if it doesn't exist
+            if( $id != null && !isset( $cities[$id] ) ) {
+                $cities[$id] = array();
+                $city = array(
+                    'code' => $data[0],
+                    'name' => $data[10],
+                    'metro_code' => $data[11]
+                );
+                array_push( $cities[$id] , $city );
+            }
+        }
+    }
+}
+
+// sort
+array_multisort( $cities, SORT_ASC ); 
+
+// output
+echo json_encode( $cities, JSON_UNESCAPED_UNICODE );
--- data/locations/provinces.php
+++ data/locations/provinces.php
@@ -1,2 +1,37 @@
-Provinces JSON:<br/>
-Country: <?php echo $_GET['c']; ?>
+<?php
+
+include( 'json-header.php' );
+
+$country = strtoupper( $_GET['c'] );
+
+$provinces = array();
+
+$row = 0;
+if ( ( $handle = fopen( $file, 'r' ) ) !== FALSE ) {
+    while ( ( $data = fgetcsv( $handle, 1000, ',' ) ) !== FALSE ) {
+        $num = count( $data );
+        $row++;
+        if( $row > 1 ) {
+            // get country code and compare
+            $cc = $data[4];
+            if( $country != $cc ) continue;
+            // get province code
+            $prov = $data[6];
+            // add province to array if it doesn't exist
+            if( $prov != null && !isset( $provinces[$prov] ) ) {
+                $provinces[$prov] = array();
+                $province = array(
+                    'code' => $data[6],
+                    'name' => $data[7]
+                );
+                array_push( $provinces[$prov] , $province );
+            }
+        }
+    }
+}
+
+// sort
+array_multisort( $provinces, SORT_ASC ); 
+
+// output
+echo json_encode( $provinces, JSON_UNESCAPED_UNICODE );
