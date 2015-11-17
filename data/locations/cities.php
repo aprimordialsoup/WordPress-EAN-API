147e851a47a8b921fc575e2c74af2ba4eda4f405
Michael Vinogradov <michael@extraed.ca>
2015-11-16 23:15:17 -0500

    changing the structure of the Cities data feed

--- data/locations/cities.php
+++ data/locations/cities.php
@@ -2,10 +2,13 @@
 
 include( 'json-header.php' );
 
+// currently selected province and country
 $province = strtoupper( $_GET['p'] );
 $country = strtoupper( $_GET['c'] );
-
+// stores city objects
 $cities = array();
+// stores cities to avoid adding duplicates
+$hash = array();
 
 $row = 0;
 if ( ( $handle = fopen( $file, 'r' ) ) !== FALSE ) {
@@ -22,21 +25,28 @@ if ( ( $handle = fopen( $file, 'r' ) ) !== FALSE ) {
             // get city code
             $id = $data[0];
             // add city to array if it doesn't exist
-            if( $id != null && !isset( $cities[$id] ) ) {
-                $cities[$id] = array();
+            if( $id != null && !in_array( $id, $hash ) ) {
+                // add city to the hash table
+                array_push( $hash, $id );
+                // add city to the cities array
                 $city = array(
                     'code' => $data[0],
                     'name' => $data[10],
                     'metro_code' => $data[11]
                 );
-                array_push( $cities[$id] , $city );
+                array_push( $cities , $city );
             }
         }
     }
 }
 
+// define custom sort function
+function sort_by_name( $a, $b ) {
+    return $a['name'] > $b['name'];
+}
+
 // sort
-array_multisort( $cities, SORT_ASC ); 
+usort( $cities, 'sort_by_name' );
 
 // output
 echo json_encode( $cities, JSON_UNESCAPED_UNICODE );
